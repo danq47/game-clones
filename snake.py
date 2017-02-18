3213a022571a45cb957df7f2c613c05c5506a63e
Dan Quill <dquill5@gmail.com>
2017-02-18 00:22:02 +0000

    Now I've got a snake which moves and eats and grows, haven't done anything about it dying yet though

--- snake.py
+++ snake.py
@@ -5,8 +5,8 @@ import math
 import random
 
 # TODO
-# 1. Figuure out how to get the tail to follow
-# 2. Implement food which grows the snake
+######## 1. Figuure out how to get the tail to follow - DONE
+######## 2. Implement food which grows the snake - DONE
 # 3. Conditions for death
 # 4. "queue" moves i.e. if I press up then lef in quick succsession, the the snake should first turn up, thenleft on its next move
 # 5. high scores
@@ -31,10 +31,12 @@ BLACK    = (   0,   0,   0)
 GREY     = ( 126, 126, 126)
 GREEN    = (   0, 255,   0)
 PURPLE   = ( 139,   0, 139)
+WHITE    = ( 255, 255, 255)
 
 # ----- Global variables ------
 
 score=0
+high_score=0
 game_over = False
 speed=[ -1 , 0 ] # (vx,vy)
 
@@ -63,24 +65,10 @@ class Snake:
         for _ in range(self.length):
             self.position.append( [START_POSITION[0] + _ , START_POSITION[1] ] )
 
-        # self.x = START_POSITION[0]
-        # self.y = START_POSITION[1]
-        
         self.vx = speed[0]
         self.vy = speed[1]
 
-
-
-
-
         
-    def update_position(self):
-
-        self.x_coord = self.x*BLOCK_SIZE + LEFT_EDGE 
-        self.y_coord = self.y*BLOCK_SIZE + UPPER_EDGE
-
-        
-
     def get_direction(self):
 
         if   self.vx ==  0 and self.vy ==  1 :
@@ -98,12 +86,22 @@ class Snake:
         x1=self.position[0][0]+self.vx
         y1=self.position[0][1]+self.vy
 
-        self.position.insert( 0 , [ x1 , y1 ] )
-        self.position.pop(-1)
+        self.position.insert( 0 , [ x1 , y1 ] ) # makes the tail follow
+        self.position.pop(-1) 
 
         return [ self.vx , self.vy ]
 
 
+    def eat(self): # same as move but without removing the final tail
+        x1=self.position[0][0]+self.vx
+        y1=self.position[0][1]+self.vy
+
+        self.position.insert( 0 , [ x1 , y1 ] )
+
+        self.length += 1
+
+        global score
+        score += 1
 
     def turn_up(self,last_move):
 
@@ -130,6 +128,23 @@ class Snake:
             self.vy =  0
 
 
+    def check_if_eating(self,x,y):
+
+        if [x,y] in self.position:
+            return True
+        else:
+            return False
+
+
+class Food:
+
+
+    def __init__(self):
+
+        self.x = random.randint( 0 , WIDTH - 1 )
+        self.y = random.randint( 0 , HEIGHT - 1 )
+
+
 
 
 
@@ -151,13 +166,27 @@ while not done:
 
 # --- Limit to 60 frames per second
     clock.tick(60)
-    
+  
+
+# ---- 1. Make the initial snake -----  
     try:
         snake
     except NameError:
         snake=Snake()
 
-    # if snake.move
+# ---- 2. make some food ------
+
+    try:
+        food
+    except NameError:
+        while True:
+            food=Food()
+            if [ food.x, food.y ] in snake.position :
+                del(food)
+                food=Food()
+            else:
+                break
+
 
     for event in pygame.event.get(): # loop over all user events
         if event.type == pygame.QUIT:
@@ -179,6 +208,11 @@ while not done:
 
 
 
+# ---- 3. Eat food, grow -----
+    if snake.check_if_eating( food.x, food.y ) :
+        del(food)
+        snake.eat()
+
 
 
 
@@ -194,11 +228,25 @@ while not done:
     pygame.draw.line(screen, GREEN, [LEFT_EDGE,  UPPER_EDGE], [RIGHT_EDGE,  UPPER_EDGE] )
 
 
-# 3. Draw the snake
+# 3. Draw the snake and food
 
     for _ in range( snake.length ):
         [x,y] = to_coords(snake.position[_][0], snake.position[_][1])
         pygame.draw.rect(screen, GREEN, [x*.88, y*.88, BLOCK_SIZE*.76, BLOCK_SIZE*.76])
+
+    try:
+        food_position=to_coords( food.x, food.y )
+        pygame.draw.rect(screen, PURPLE, [food_position[0]*.88, food_position[1]*.88 , BLOCK_SIZE*.76, BLOCK_SIZE*.76 ] )
+    except NameError:
+        pass
+
+# 4. Draw scores
+    score_to_print = font.render("SCORE:"+str(score),True,WHITE)
+    high_score_to_print = font.render("HIGH SCORE:"+str(high_score),True,WHITE)
+    score_width = score_to_print.get_rect().width
+    high_score_width = high_score_to_print.get_rect().width
+    screen.blit(score_to_print, [ (XMAX/2 - score_width )/2 , BLOCK_SIZE ] )
+    screen.blit(high_score_to_print, [ (XMAX/2 - high_score_width )/2 + XMAX/2 , BLOCK_SIZE ] )
 
     pygame.display.flip()
 
